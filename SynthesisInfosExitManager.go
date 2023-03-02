d2c8af485188a16731ea932953dee855b2d2e87d
Steeve Descarpentries <s.descarpentries@octo.com>
2023-03-02 17:20:50 +0100

    Déplacement de TokenRevoker.go dans la clean archi.

--- SynthesisInfosExitManager.go
+++ SynthesisInfosExitManager.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"Octoptimist/dataproviders"
 	"log"
 	"net/http"
 )
 
 func (infos SynthesisInfos) manageExit(r *http.Request, w http.ResponseWriter) {
 	if len(r.Form["btnExit"]) > 0 {
-		err := TokenRevoker(infos.AccessToken)
+		err := dataproviders.TokenRevoker(infos.AccessToken)
 		if err != nil {
 			log.Printf("error revoking token : %s\n", err)
 		}
